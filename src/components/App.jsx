f2e9fa935874505892c8504d66750cb5a646d417
Tetiana Zorii <tanyusha.zoriy@gmail.com>
2024-02-28 14:24:19 -0500

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,69 +6,59 @@ import { ContactList } from './ContactList/ContactList';
 
 import css from './App.module.css';
 
-
-// const initialContacts = [
-//   { id: 'id-1', name: 'Harper Rodriguez', number: '459-12-56' },
-//   { id: 'id-2', name: 'Mia Chen', number: '443-89-12' },
-//   { id: 'id-3', name: 'Donovan Patel', number: '645-17-79' },
-//   { id: 'id-4', name: 'Isabella Santiago', number: '227-91-26' },
-// ];
+const STORAGE_KEY = 'contacts';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Beyoncé', number: '342-12-44' },
-      { id: 'id-2', name: 'Drake', number: '440-22-78' },
-      { id: 'id-3', name: 'Taylor Swift', number: '897-11-20' },
-      { id: 'id-4', name: 'Shawn Mendes', number: '284-91-51' },
-    ],
+    contacts: [],
     filter: '',
   };
 
-   componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+  componentDidMount() {
+    const storageValue = localStorage.getItem(STORAGE_KEY);
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
+    if (storageValue) {
+      this.setState({
+        contacts: JSON.parse(storageValue),
+      });
     }
   }
 
-  componentDidUpdate(_, prevState) {
+  componentDidUpdate(prevProps, prevState) {
     if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.state.contacts));
     }
   }
 
-  handleChange = evt => {
+  handleChange = (evt) => {
     this.setState({ [evt.target.name]: evt.target.value });
   };
 
   createContact = (name, number) => {
     if (
       this.state.contacts.some(
-        contact => contact.name.toLowerCase() === name.toLowerCase()
+        (contact) => contact.name.toLowerCase() === name.toLowerCase()
       )
     ) {
       alert('exist');
       return;
     }
-    
-    this.setState(prev => ({
+
+    this.setState((prev) => ({
       contacts: [...prev.contacts, { name, number, id: nanoid() }],
     }));
   };
 
   filterContacts = () => {
     const { contacts, filter } = this.state;
-    return contacts.filter(contact =>
+    return contacts.filter((contact) =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
-  }
+  };
 
-  handleDelete = id => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(contact => contact.id !== id),
+  handleDelete = (id) => {
+    this.setState((prev) => ({
+      contacts: prev.contacts.filter((contact) => contact.id !== id),
     }));
   };
 
